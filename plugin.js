c8b7c41607346649306917751c6dc5552ef1eb56
keyanmit <keyankarthi.mit@gmail.com>
2014-08-10 01:25:23 +0530

    css updated, dom update

--- plugin.js
+++ plugin.js
@@ -6,26 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
 //template stuff
   
   //fuck u bitch
-  window.template = new function(){
+  window.template = new function(){      
 
-      var tmplText= '<div class="TorrentList">'
-                    + '<div class="Torrent">'
-                      + '<span class="Quality">${Quality}</span>'
-                      + '<span class="SeedPeerRatio">${TorrentSeeds}/${TorrentPeers}</span>'
-                      + '<span class="TorrentSize">${Size}</span>'
-                    + '</div>'
-                  + '</div>';
+      var movieTplHtml = '<div class="movie">'
+                              + '<div class="MovieBanner">'
+                                + '<div class="DescWrapper">'
+                                  + '<div class="Title">${MovieTitle}</div>'
+                                + '</div>'
+                                + '<div class="TorrentList">'
+                                  + '<div class="Torrent">'
+                                    + '<span class="RatingStar">4.8</span>'
+                                  + '</div>'
+                                + '</div>'
+                              + '</div>'
+                          + '</div>';
 
-      var bannerText = '<div class="MovieBanner">'
-                        + '<div class="DescWrapper">'
-                        +   '<div class="Title">${MovieTitle}</div>'
-                        +   '<div class="TorrentCount">${TorrentCount} torrents.</div>'
-                        + '</div>'
-                        + '<div class="RatingStar"></div>'
-                      + '</div>';              
+      var torentListTmplHtml = '<span class="Quality" title="Seed/Peers : ${TorrentSeeds} / $(TorrentPeers) Size : ${Size}" >${Quality}</span>';
 
-      this.TorrentBanner = $.template("comiledBannerTxt",bannerText); 
-      this.TorrentList = $.template("compiledTmplTxt",tmplText);   
+      this.MovieTmpl = $.template("compiledMovieTemplate",movieTplHtml);
+      this.TorrentTmpl = $.template("compiledTorrentTmpl",torentListTmplHtml);
   };
 
   
@@ -44,13 +43,12 @@ function show(){
 
       $('.TorrentContainer').html("");//clear the exising torrents
       
-      $.tmpl(window.template.TorrentBanner,{
-        MovieTitle : name,
-        TorrentCount : count
+      $.tmpl(window.template.MovieTmpl,{
+        MovieTitle : name        
       }).appendTo('.TorrentContainer');
 
-      $.tmpl(window.template.TorrentList,movies)
-        .appendTo('.TorrentContainer');                
+      $.tmpl(window.template.TorrentTmpl,movies)
+        .appendTo('.TorrentContainer .Torrent');                
   });
 }
 
@@ -67,13 +65,12 @@ window.UI = new function(){
             //show no result found. exit
             return;
           }
-          $.tmpl(window.template.TorrentBanner,{
-            MovieTitle : name,
-            TorrentCount : count
-          }).appendTo('.TorrentContainer'); 
+          $.tmpl(window.template.MovieTmpl,{
+            MovieTitle : name        
+          }).appendTo('.TorrentContainer');
 
-          $.tmpl(window.template.TorrentList,movies)
-          .appendTo('.TorrentContainer');
+          $.tmpl(window.template.TorrentTmpl,movies)
+            .appendTo($('.TorrentContainer .Torrent')[idx]);     
         }); 
       });
     }
